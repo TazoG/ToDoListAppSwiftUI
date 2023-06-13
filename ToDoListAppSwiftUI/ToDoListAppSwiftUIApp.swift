7ff36705dc649903d173123ffd5a666ab8344791
Tazo <tazogigitashvili@gmail.com>
2023-06-13 20:59:32 +0400

    Initial Commit

---
+++ ToDoListAppSwiftUI/ToDoListAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppSwiftUIApp.swift
+//  ToDoListAppSwiftUI
+//
+//  Created by Tazo Gigitashvili on 13.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
