73964ee67a9c8e6ca649344c46237994ec006f14
Tazo <tazogigitashvili@gmail.com>
2023-06-14 23:05:57 +0400

    Created users by firebase, first log in

--- ToDoListAppSwiftUI/ViewModels/MainViewViewModel.swift
+++ ToDoListAppSwiftUI/ViewModels/MainViewViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by Tazo Gigitashvili on 13.06.23.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        Auth.auth().currentUser != nil
+    }
+}
