b29d3b7df641feff8074667e1f4208c700b752b6
Tazo <tazogigitashvili@gmail.com>
2023-06-17 21:23:29 +0400

    Done checkmarks

--- ToDoListAppSwiftUI/ViewModels/ProfileViewViewModel.swift
+++ ToDoListAppSwiftUI/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Tazo Gigitashvili on 13.06.23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
@@ -12,6 +14,16 @@ class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
